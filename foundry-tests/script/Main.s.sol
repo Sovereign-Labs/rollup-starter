58b7b4e493cf50dac4df1be95c961852b823f7b1
Leonid Logvinov <logvinov.leon@gmail.com>
2026-01-02 15:18:23 +0100

    Rename Counter to Main

--- foundry-tests/script/Main.s.sol
+++ foundry-tests/script/Main.s.sol
@@ -2,17 +2,17 @@
 pragma solidity ^0.8.13;
 
 import {Script} from "forge-std/Script.sol";
-import {Counter} from "../src/Counter.sol";
+import {Main} from "../src/Main.sol";
 
-contract CounterScript is Script {
-    Counter public counter;
+contract MainScript is Script {
+    Main public main;
 
     function setUp() public {}
 
     function run() public {
         vm.startBroadcast();
 
-        counter = new Counter();
+        main = new Main();
 
         vm.stopBroadcast();
     }
