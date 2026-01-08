d068ae4951655394da11d0916aa10a34fd0e774a
Leonid Logvinov <logvinov.leon@gmail.com>
2026-01-08 15:05:48 +0100

    Use private key instead of unlocked sender in run.sh

--- foundry-tests/run.sh
+++ foundry-tests/run.sh
@@ -10,7 +10,6 @@ TEST_NAME=${1:-AllTests}
 forge script \
     "$TEST_NAME" \
     --rpc-url sovereign \
-    --sender 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266 \
-    --unlocked \
+    --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 \
     --broadcast \
     --code-size-limit 524288
