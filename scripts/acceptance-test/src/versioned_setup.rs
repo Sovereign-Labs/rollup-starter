00e342eea63339f4c7e7bbfc06ea47e289405653
Theodore Bugnet <theodore@sovlabs.io>
2026-03-18 02:09:09 +0000

    Fix starting salt to prevent nonce issues

--- scripts/acceptance-test/src/versioned_setup.rs
+++ scripts/acceptance-test/src/versioned_setup.rs
@@ -19,7 +19,7 @@ pub const VERSION_SPEC_FILE: &str = "versions.yaml";
 pub const VERSION_VARS_COMMIT_KEY: &str = "rollup_commit_hash";
 pub const VERSION_CONFIG_TEMPLATE_PATH: &str = "scripts/acceptance-test/rollup_config.toml";
 pub const SOAK_NUM_WORKERS: u32 = 20;
-pub const SOAK_SALT: u32 = 0;
+pub const SOAK_SALT: u32 = 3; // existing acceptance-test-data started from 3 for some reason
 pub const SOAK_SAFETY_STOP_BLOCKS: u64 = 5;
 const ACCEPTANCE_TEST_FEATURES: [&str; 3] = ["acceptance-testing", "mock_da", "mock_zkvm"];
 
