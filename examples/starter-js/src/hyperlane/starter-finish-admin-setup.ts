48a873373086e434fb96f75943bea5bf9f1de570
Preston Evans <preston.b.evans@gmail.com>
2026-08-07 20:10:43 +0200

    Add another deployer

--- examples/starter-js/src/hyperlane/starter-finish-admin-setup.ts
+++ examples/starter-js/src/hyperlane/starter-finish-admin-setup.ts
@@ -30,6 +30,7 @@ const evmAdminCall: RuntimeCall = {
                         // "0xf8ffaA5c3812659D832Ea866526F76A3B7F3b298"
                         // "0x71d8bE89D9f2339F0FE9cBA39496C6C9cbFF9da6",
                         // "0xbE595DaD4d75cf04314e6e217F379926aFD38244",
+                        "0x8636752e1458b6B07318B9Cd53f5CbDD0ef3FEc3",
                       ],
                     remove: [],
                 },
